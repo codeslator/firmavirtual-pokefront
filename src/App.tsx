8fa9c51a16a11a2131e0d575bfb1fcb2b831ddc1
Andrés Meléndez <codeslator@gmail.com>
2024-06-05 14:09:28 -0400

    Disable react query devtools;

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { RouterProvider } from "react-router-dom"
 import { router } from "./router"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
@@ -9,7 +9,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
-      <ReactQueryDevtools initialIsOpen={false} />
+      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
     </QueryClientProvider>
   )
 }
